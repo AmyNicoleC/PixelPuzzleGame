896da3a47d8928f08862af2529fc976738db583b
AmyNicoleC <47595876+AmyNicoleC@users.noreply.github.com>
2019-02-13 12:08:09 +0100

    Committing v0.1 of the Pixel Puzzle Game

---
+++ sketch.js
@@ -0,0 +1,107 @@
+var activePuzzle
+var cellWidth = 32
+var puzzleBorder = 64
+var border
+var mouseClickActive = false
+var activateCell = false
+
+function setup() {
+  createCanvas(640, 480)
+  frameRate(30)
+  activePuzzle = new Puzzle
+  var puzzle = activePuzzle
+  puzzle.width = 10
+  puzzle.height = 10
+  border = min(4, round(cellWidth * 0.125))
+  //create cells
+  for(var i = 0; i < puzzle.width*puzzle.height; i++) {
+    puzzle.cells[puzzle.cells.length] = new Cell
+  }
+  // set cell values
+  precalcCells(puzzle)
+  setupLinearCountArray(puzzle)
+}
+
+function draw() {
+  background(224)
+  puzzleBorder = ceil(max(activePuzzle.width, activePuzzle.height)/2) * 16 + 16
+  border = min(4, round(cellWidth * 0.125))
+  cellActivation()
+  recalcCells(activePuzzle)
+  recalcLinearCountArray(activePuzzle)
+  drawCells(activePuzzle)
+  drawValues(activePuzzle)
+}
+
+/*function mouseClicked() {
+  var puzzleW = activePuzzle.width
+  var puzzleH = activePuzzle.height
+  for(var i = 0; i < puzzleW*puzzleH; i++) {
+    var cell = activePuzzle.cells[i]
+
+    var x = cell.x
+    var y = cell.y
+    var w = cell.width
+
+    if(mouseX >= x && mouseX < x+w){
+      if(mouseY >= y && mouseY < y+w){
+        cell.active = !cell.active
+      }
+    }
+  }
+}
+*/
+
+function mousePressed(event) {
+  if(event.button == 0) {
+    mouseClickActive = true
+
+    var puzzleW = activePuzzle.width
+    var puzzleH = activePuzzle.height
+    for(var i = 0; i < puzzleW*puzzleH; i++) {
+      var cell = activePuzzle.cells[i]
+
+      var x = cell.x
+      var y = cell.y
+      var w = cell.width
+
+      if(mouseX >= x && mouseX < x+w){
+        if(mouseY >= y && mouseY < y+w){
+          setCellActive = !activePuzzle.cells[i].active
+        }
+      }
+    }
+  }
+}
+
+
+
+function mouseReleased(event) {
+  mouseClickActive = false
+}
+
+function cellActivation() {
+  if(mouseClickActive) {
+    var puzzleW = activePuzzle.width
+    var puzzleH = activePuzzle.height
+    for(var i = 0; i < puzzleW*puzzleH; i++) {
+      var cell = activePuzzle.cells[i]
+
+      var x = cell.x
+      var y = cell.y
+      var w = cell.width
+
+      if(mouseX >= x && mouseX < x+w){
+        if(mouseY >= y && mouseY < y+w){
+          cell.active = setCellActive
+        }
+      }
+    }
+    fill(0,255,0)
+    rect(4,4,24,24)
+  }
+  else {
+    fill(255,0,0)
+    rect(4,4,24,24)
+  }
+}
---
+++ visualisation.js
@@ -0,0 +1,101 @@
+//draws out the border and right shape of the cells
+function drawCells(puzzle) {
+  for(var i = 0; i < puzzle.width * puzzle.height; i++) {
+    noFill(0)
+    var shape = 0//round(random(0,4))
+    var cell = puzzle.cells[i]
+    cell.width = cellWidth
+    var x = cell.x
+    var y = cell.y
+    var w = cell.width
+    var b = border
+
+    fill(255)
+    rect(x, y, w, w)
+    noFill()
+    var x1 = x + b * 1.25
+    var x2 = x + w - b * .75
+    var y1 = y + b * 1.25
+    var y2 = y + w - b * .75
+
+    if(cell.active) {
+      fill(0)
+      switch (shape) {
+        case 0:
+          rect(x+border, y+border, w-border*2, w-border*2)
+          break;
+        case 1:
+          triangle(x1, y1, x2, y1, x1, y2)
+          break;
+        case 2:
+          triangle(x1, y1, x2, y1, x2, y2)
+          break;
+        case 3:
+          triangle(x1, y1, x1, y2, x2, y2)
+          break;
+        case 4:
+          triangle(x2, y1, x1, y2, x2, y2)
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
+
+//draws out the values of the horizonal and vertical linear counting arrays
+function drawValues(puzzle) {
+  fill(0)
+  stroke(0)
+  strokeWeight(.5)
+  for(var i = 0; i < puzzle.height; i++) {
+    for(var j = 0; j < puzzle.horizonalLinearCounters[i].values.length; j++) {
+      var horLinCount = puzzle.horizonalLinearCounters[i].values[j]
+      var x = 16 + 16 * j
+      var y = puzzle.cells[1 + i * puzzle.width].y + cellWidth / 2
+      strokeWeight(1)
+      fill(255)
+      rect(x - 8, y - 8, 16, 16)
+      strokeWeight(.5)
+      fill(0)
+      textAlign(CENTER, CENTER)
+      text(horLinCount, x, y)
+      noFill()
+    }
+    strokeWeight(1)
+    fill(255, 0, 0)
+    rect(
+      puzzle.cells[i * puzzle.width].x-6,
+      puzzle.cells[i * puzzle.width].y,
+      6,
+      cellWidth)
+    noFill()
+    strokeWeight(.5)
+  }
+
+  for(var i = 0; i < puzzle.width; i++) {
+    for(var j = 0; j < puzzle.verticalLinearCounters[i].values.length; j++) {
+      var vertLinCount = puzzle.verticalLinearCounters[i].values[j]
+      var x = puzzle.cells[i].x + cellWidth / 2
+      var y = 16 + 16 * j
+      fill(255)
+      strokeWeight(1)
+      rect(x - 8, y - 8, 16, 16)
+      fill(0)
+      strokeWeight(.5)
+      textAlign(CENTER, CENTER)
+      text(vertLinCount, x, y)
+      noFill()
+    }
+    strokeWeight(1)
+    fill(0, 255, 0)
+    rect(
+      puzzle.cells[i].x,
+      puzzle.cells[i].y-6,
+      cellWidth,
+      6)
+    noFill()
+    strokeWeight(.5)
+  }
+  strokeWeight(1)
+}
